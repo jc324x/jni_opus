15750b542fd6c2dc4337524b94fbfc75e55b4d54
Jon Christensen <jon.christensen@compozelabs.com>
2024-04-18 06:51:43 -0500

    macos/windows split make includes

--- jni_opus_headers_macos.mk
+++ jni_opus_headers_macos.mk
@@ -2,7 +2,7 @@ OPUS_HEAD = \
 include/opus.h \
 include/opus_jni.h\
 include/jni.h\
-include/windows/jni_md.h\
+include/macos/jni_md.h\
 include/opus_multistream.h \
 include/opus_projection.h \
 src/opus_private.h \
--- jni_opus_headers_windows.mk
+++ jni_opus_headers_windows.mk
@@ -2,7 +2,7 @@ OPUS_HEAD = \
 include/opus.h \
 include/opus_jni.h\
 include/jni.h\
-include/jni_md.h\
+include/windows/jni_md.h\
 include/opus_multistream.h \
 include/opus_projection.h \
 src/opus_private.h \
